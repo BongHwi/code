cb532a6e110636852d892cd16ad3366f2db2b02c
Bong-Hwi Lim <bonghwilim@gmail.com>
2016-04-09 10:02:51 +0900

    235685->235226

--- RunGrid.C
+++ RunGrid.C
@@ -138,7 +138,7 @@ AliAnalysisGrid* CreateAlienHandler(TString pluginmode="test",
 //        plugin->AddRunNumber(225763); // data only
     
 //        nruns++;
-    plugin->AddRunNumber(235685); // data only
+    plugin->AddRunNumber(235226); // data only
     
     nruns++;
     
@@ -146,7 +146,7 @@ AliAnalysisGrid* CreateAlienHandler(TString pluginmode="test",
     plugin->SetNrunsPerMaster(nruns);
     
     // Define alien work directory where all files will be copied. Relative to alien $HOME.
-    plugin->SetGridWorkingDir("RunByRunStudy2/LHC15i_Run235685_codetest_20160409_01/");
+    plugin->SetGridWorkingDir("RunByRunStudy2/LHC15i_Run235226_codetest_20160409_01/");
     plugin->SetGridOutputDir("output");
     plugin->AddIncludePath("-I. -I$ROOTSYS/include -I$ALICE_ROOT -I$ALICE_ROOT/include -I$ALICE_ROOT/CONTAINERS -I$ALICE_ROOT/STEER -I$ALICE_ROOT/TOF -I$ALICE_ROOT/macros -I$ALICE_ROOT/ANALYSIS -I$ALICE_PHYSICS -I$ALICE_PHYSICS/include");
     
